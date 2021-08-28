e4616d450df0e63af175780723d7916a0c85d3a0
joelthecoder <joelthecoder@gmail.com>
2021-08-28 11:36:35 -0400

    Add ability to run in repl mode

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
+import { ReplServer } from './core/repl';
+
 console.log(`🚀 And we're live!`);
+
+/**
+ * Run the repl server if the environment variable "USE_REPL" is set to true
+ */
+if (process.env.USE_REPL === 'true') {
+  const server = new ReplServer();
+  server.addContext('test', 123);
+}
